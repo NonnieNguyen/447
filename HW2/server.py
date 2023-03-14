5f787f6e435e0e6cff95fd856d57c582a1212f05
NonnieNguyen <alannguyen514@gmail.com>
2023-03-14 14:36:57 -0400

    Created Search and Remove function
    removed file removedresult.html

--- HW2/server.py
+++ HW2/server.py
@@ -16,31 +16,36 @@ def remove():
    cur.execute("select * from students")
    
    rows = cur.fetchall()   
-   
+   con.close()
    return render_template("remove.html",rows = rows)
 
-@app.route('/removerec',methods = ['POST', 'GET'])
+@app.route('/removerec', methods = ['POST', 'GET'])
 def removerec():
    if request.method == 'POST':
       try:
          nm = request.form['nm']
-         id = request.form['id']
-         point = request.form['point']
          
          with sql.connect("database.db") as con:
             cur = con.cursor()
-            
-            cur.execute("DELETE FROM students WHERE nm =(?)",(nm))
-            
-            con.commit()
-            msg = "Record successfully removed"
+            cur.execute("SELECT name FROM students WHERE name=?",(nm,))
+
+            if cur.fetchone():
+               cur.execute("DELETE FROM students WHERE name=?",(nm,))
+               con.commit()
+               msg = "Record successfully removed"
+
+            else:
+               con.rollback()
+               msg = "Could not find user"
+
       except:
          con.rollback()
          msg = "error in removing operation"
       
       finally:
-         return render_template("removedresult.html",msg = msg)
+
          con.close()
+         return render_template("result.html",msg = msg)
 
 
 @app.route('/enternew')
@@ -67,8 +72,8 @@ def addrec():
          msg = "error in insert operation"
       
       finally:
-         return render_template("result.html",msg = msg)
          con.close()
+         return render_template("result.html",msg = msg)
 
 @app.route('/list')
 def list():
@@ -79,7 +84,43 @@ def list():
    cur.execute("select * from students")
    
    rows = cur.fetchall()
+   con.close()
    return render_template("list.html",rows = rows)
+
+@app.route('/search')
+def search():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall()
+   con.close()
+   return render_template("search.html",rows = rows)
+
+@app.route('/searchrec',methods = ['POST', 'GET'])
+def searchrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         id = request.form['id']
+         
+         con = sql.connect("database.db")
+         con.row_factory = sql.Row
+
+         cur = con.cursor()
+         cur.execute("SELECT * FROM students WHERE name = ? OR id = ?", (nm,id))
+
+         rows = cur.fetchall()
+         msg = "All User(s) named " + nm +" and User(s) with ID of " + id
+      except:
+         con.rollback()
+         msg = "User was not found"
+      
+      finally:
+         con.close()
+         return render_template("sresult.html",msg = msg, rows = rows)
 
 if __name__ == '__main__':
    conn = sql.connect('database.db')
